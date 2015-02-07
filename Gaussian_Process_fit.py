96d0717cffd91bd2b97c0c1d86cbfa37db95cdf8
Daniele <Dragoni-Daniele@users.noreply.github.com>
2015-02-07 19:24:26 +0100

    Update Gaussian_Process_fit.py

--- Gaussian_Process_fit.py
+++ Gaussian_Process_fit.py
@@ -146,8 +146,7 @@ def  GP_fit(x_prediction,x_training,t_training,Theta,Lambda,Sigma):
    posterior_mean_der1_noisy = posterior_mean_der1_noisy+poly_func_der1(x_prediction)
    posterior_mean_der2_noisy = posterior_mean_der2_noisy+poly_func_der2(x_prediction)
    
-   if x_training is float:
-         return posterior_mean[0],posterior_mean_der1[0],posterior_mean_der2[0], posterior_mean_noisy[0],posterior_mean_der1_noisy[0],posterior_mean_der2_noisy[0]
-   else:
-         return posterior_mean,posterior_mean_der1,posterior_mean_der2, posterior_mean_noisy,posterior_mean_der1_noisy,posterior_mean_der2_noisy
+
+   return posterior_mean[0],posterior_mean_der1[0],posterior_mean_der2[0], posterior_mean_noisy[0],posterior_mean_der1_noisy[0],posterior_mean_der2_noisy[0]
+
    print 'Execution time: ',(datetime.now()-startTime)
